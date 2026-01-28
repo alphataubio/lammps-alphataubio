f022e88fe28083ae39e11748667bf2b0ab7e15c4
alphataubio <alphataubio@gmail.com>
2026-01-28 11:08:49 -0500

    Update write_psf.h

--- src/CHARMM/write_psf.h
+++ src/CHARMM/write_psf.h
@@ -41,15 +41,6 @@ class WritePsf : public Command {
   bigint nangles_local, nangles;
   bigint ndihedrals_local, ndihedrals;
   bigint nimpropers_local, nimpropers;
-  
-  struct psf_atom {
-    tagint tag, molecule;
-    int type;
-    double q;
-    char segment[9], residue[9], name[9];
-  };
-  std::vector<psf_atom> psf_atoms;
-  
   std::unordered_map<tagint, tagint> tag_to_index;
 
   void header();
